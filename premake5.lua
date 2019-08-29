dc18c94d807a9cdb4c0350c10769f52a3856459c
Noah Forberich <Cr4fter@users.noreply.github.com>
2019-08-29 14:09:11 +0200

    Changed to static lib

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,12 @@
 project "ImGui"
     kind "StaticLib"
     language "C++"
-    
+    cppdialect "C++17"
+	staticruntime "on"
 	targetdir ("bin/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}")
     objdir ("bin-int/%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}")
-
+	systemversion "latest"
+	
 	files
 	{
         "imconfig.h",
@@ -18,12 +20,10 @@ project "ImGui"
         "imstb_truetype.h",
         "imgui_demo.cpp"
     }
-    
-	filter "system:windows"
-        systemversion "latest"
-        cppdialect "C++17"
-        staticruntime "On"
-		defines "IMGUI_API=__declspec(dllexport)"
-        
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
